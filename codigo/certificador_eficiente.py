1650f774ae576955894d96e564c7ad7710087470
Sofia-gb <sofiagomezb@yahoo.com.ar>
2024-05-30 12:11:44 -0300

    Código del certificador y justificación en el informe

---
+++ codigo/certificador_eficiente.py
@@ -0,0 +1,39 @@
+def validador_problema_tribu_del_agua(habilidades_maestros_agua, k, B, S):
+    if len(S) != k: #O(1)
+        return False 
+    suma = 0 #O(1)
+    maestros = set() #O(1) Cada maestro debe estar en solo un grupo
+    dic_habilidades = dict() #O(1)
+    
+    for info_maestro in habilidades_maestros_agua: #O(n)
+        maestro, habilidad = info_maestro #O(1)
+        dic_habilidades[maestro] = habilidad #O(1)
+    
+    # O(n * k)
+    for grupo in S: # k grupos: O(k)
+        suma_grupo = 0 #O(1)
+        for maestro in grupo: # En el peor caso O(n)
+            if maestro in maestros or maestro not in dic_habilidades:
+                return False # El maestro dado está en más de un grupo
+            maestros.add(maestro) #O(1)
+            suma_grupo += dic_habilidades[maestro] #O(1)
+        suma += suma_grupo ** 2 #O(1)
+            
+    if suma > B:  # O(1)
+        return False
+        
+    for maestro, _habilidad in habilidades_maestros_agua: #O(n)
+        if maestro not in maestros:  # O(1)
+            return False # No tiene grupo asignado
+        
+    return True
+    
+# T(n) = O(1) + O(k * n) + 2*O(n) = O(k * n). En el peor caso k=n (1 maestro por grupo) y sería O(n^2)
+        
+k = 3
+habilidades = [("Hasook", 120), ("Hama", 445), ("Senna", 546), ("Hama I", 222), ("Wei", 551), ("Wei I", 330)] 
+S = [{"Wei", "Hasook"}, {"Senna", "Hama I"}, {"Hama", "Wei I"}]
+B = 1640690
+
+print(validador_problema_tribu_del_agua(habilidades, k, B, S))
+#cada grupo tiene menos de n maestros necesariamente, pero en el peor caso, si k=1, tendrá a todos: O(n)
