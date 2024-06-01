601bfb882f1d3efb7a14aaf2809c1a5410da5207
Sofia-gb <sofiagomezb@yahoo.com.ar>
2024-06-01 14:14:01 -0300

    Ordenar + tests hasta 15_5.txt. Hasta ahora el de 14_6.txt es el que más tarda

--- codigo/backtracking.py
+++ codigo/backtracking.py
@@ -3,7 +3,7 @@ def problema_tribu_del_agua_bt(maestros_y_habilidades, k):
         return None
     
     S = [set() for _ in range(k)]
-    
+    maestros_y_habilidades = sorted(maestros_y_habilidades, key=lambda x: -x[1])
     S_con_habilidades, coeficiente = problema_tribu_del_agua_bt_recur(maestros_y_habilidades, k, 0, list(S), list(S), float('inf'))
 
     resultado = []
@@ -42,27 +42,3 @@ def sumatoria(grupos):
             suma_grupo += habilidad
         suma += suma_grupo ** 2
     return suma
-
-k = 3
-habilidades = [("Hasook", 120), ("Hama", 445), ("Senna", 546), ("Hama I", 222), ("Wei", 551), ("Wei I", 330)] 
-        
-#print(problema_tribu_del_agua_bt(habilidades, k))
-""" 
-Grupo 1: Wei, Hasook
-Grupo 2: Senna, Hama I
-Grupo 3: Hama, Wei I
-Coeficiente: 1640690
-"""
-#10_3.txt
-k = 3
-h = [("Sangok", 178),("Wei", 121), ("Siku", 135), ("Eska", 168), ("Amon", 65), ("La", 37), ("Yue", 94), 
-     ("Pakku", 91), ("Sura", 159), ("Tonraq", 27)]
-""" 
-Grupo 1: Sangok, Yue, Pakku - igual
-Grupo 2: Eska, Wei, Amon - 354
-Grupo 3: Sura, Siku, La, Tonraq - 358
-Coeficiente: 385249
-rta: ([{'Pakku', 'Sangok', 'Yue'}, {'La', 'Siku', 'Wei', 'Amon'}, {'Eska', 'Tonraq', 'Sura'}], 385249)
-"""
-
-#print(problema_tribu_del_agua_bt(h, k))
